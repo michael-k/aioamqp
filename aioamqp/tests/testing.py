429c698f6c34c4076c002f59903b4aba52cc2c7e
Rémi Cardona <remi.cardona@polyconseil.fr>
2017-04-13 22:57:41 +0200

    WIP debug pending task WIP

--- aioamqp/tests/testing.py
+++ aioamqp/tests/testing.py
@@ -39,6 +39,7 @@ def coroutine(func):
         coro = asyncio.coroutine(func)
         timeout_ = getattr(func, '__timeout__', self.__timeout__)
         self.loop.run_until_complete(asyncio.wait_for(coro(self), timeout=timeout_, loop=self.loop))
+        import gc; gc.collect()
         if len(handler.messages) != 0:
             raise AsyncioErrors(handler.messages)
     return wrapper
